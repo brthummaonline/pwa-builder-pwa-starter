c7e2d61a16da7db1a0098e87cbda9068a897affe
brthummaonline <brthummaonline@users.noreply.github.com>
2022-11-23 14:27:33 +0000

    deploy: 3b2198132cea1f024a9a5a6321f069819c806698

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/pwa-builder-pwa-starter/sw.js', { scope: '/pwa-builder-pwa-starter/' })})}
